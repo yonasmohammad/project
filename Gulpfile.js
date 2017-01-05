42656c97b215c3056cb94c04203138fe391193cf
yonas.mohammad <toro.yonas.mohammad@gmail.com>
2017-01-05 17:14:00 +0800

    commit

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,21 +1,33 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const nunjucks = require('gulp-nunjucks');
 const bourbon = require("bourbon").includePaths;
+const nunjucks = require('gulp-nunjucks');
 
-gulp.task('sass', function () {
-  return gulp.src('src/scss/app.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('app/assets/css'));
+gulp.task('nunjucks',  function () {
+    gulp.src('src/templates/*.html')
+        .pipe(nunjucks.compile())
+        .pipe(gulp.dest('app/views'))
 });
-
-gulp.task('default',  function () {
-    gulp.src('src/templates/home.html')
-        .pipe(nunjucks.compile({name: 'Sindre'}))
-        .pipe(gulp.dest('app'))
-});
-
-
+gulp.task('js', function() {
+    return gulp.src([
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/bootstrap/node_modules/jquery/dist/jquery.min.js',
+        'node_modules/bootstrap/node_modules/tether/dist/js/tether.js',
+        'src/templates/js/paralex.js',
+        'src/templates/js/action.js'
+      ])
+      .pipe( gulp.dest('app/assets/js'));
+  });
+ 
+gulp.task('sass', function() {
+    return gulp.src([
+        'src/scss/app.scss',
+        'node_modules/bootstrap/font.css',
+        'node_modules/bootstrap/dist/css/bootstrap.min.css'
+      ])
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('app/assets/css'));
+  });
 
 // setup our Sass compilation task
 gulp.task('scss', function() {
@@ -27,15 +39,14 @@ gulp.task('scss', function() {
         .pipe(gulp.dest('app/assets/css'));
 });
 
-
-
 gulp.task('sass:watch', function () {
   gulp.watch(['src/scss/*.scss', 'src/scss/**/*.scss'], ['sass']);
 });
 
-gulp.task('jucks:watch', function () {
-  gulp.watch(['src/*.html', 'src/**/*.html'], ['jucks']);
+gulp.task('gulp:watch', function () {
+  gulp.watch(['src/scss/*.scss', 'src/scss/**/*.scss'], ['sass']);
+  gulp.watch(['src/templates/*.html', 'src/templates/**/*.html'], ['nunjucks']);
 });
 
 
-gulp.task('default', ['sass'])
+gulp.task('default', ['nunjucks','sass','js'])
---
+++ src/templates/js/connector.js
@@ -0,0 +1,41 @@
+ <script class="localJSAssets" src="app/assets/js/tether.js"></script>
+ <script src="https://unpkg.com/huebee@1/dist/huebee.pkgd.min.js"></script>
+ <script class="localJSAssets" src="app/assets/js/jquery.min.js"></script>
+ <script class="localJSAssets" src="app/assets/js/paralex.js"></script>
+ <script class="localJSAssets" src="app/assets/js/bootstrap.min.js"></script>
+ <script class="localJSAssets" src="Gulpfile.js"></script>
+ <script class="localJSAssets" src="app/assets/js/action.js"></script>
+     <script>
+      var directoryName = 'project/'.toLowerCase();
+      var baseUrl = window.location.href.slice(0, window.location.href.indexOf(directoryName)) + directoryName; 
+         window.onload = function() {
+        var links = document.getElementsByClassName('localLinks'); 
+        var scripts = document.getElementsByClassName('localJSAssets');
+        var images = document.getElementsByTagName('img');
+        var allScripts = [];
+        
+        for (var i = 0; i < images.length; i++) {
+          images[i].src = `${baseUrl}${images[i].getAttribute('src')}`;
+        }
+        
+        for (var i = 0; i < links.length; i++) {
+          links[i].href = baseUrl + links[i].getAttribute('href');
+        }
+        
+        for (var i = 0; i < scripts.length; i++) {
+          allScripts.push(scripts[i]);
+        }
+        
+        for (var i = 0; i < allScripts.length; i++) {
+       
+          var script = document.createElement('script'); 
+          script.src = baseUrl + allScripts[i].getAttribute('src');
+          // script.classList.add('localJSAssets')
+          document.getElementsByTagName('body')[0].appendChild(script);
+          var paras = document.getElementsByClassName('localJSAssets');
+          while(paras[0]) {
+              paras[0].parentNode.removeChild(paras[0]);
+          }
+        }
+      }
+  </script>
