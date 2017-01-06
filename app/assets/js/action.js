c43f1f6d26bb3c0c46c87ad2bb342e6210a012a5
yonas.mohammad <toro.yonas.mohammad@gmail.com>
2017-01-06 17:13:10 +0800

    initial commit

--- app/assets/js/action.js
+++ app/assets/js/action.js
@@ -1,45 +1,99 @@
+$('#picker').on('change', function(){
+    $('.edit-me').css('background', $(this).val());
+})
 
-// $("#colorChoice").change(function(){
-//   $(".custom").css('background', $(this).val());
-// });
 
-$( ".target" ).click(function() {
-  alert( "click me" );
-  console.log("s");
+
+
+$( "button" ).on('click',function() {
+    $("button").removeClass('edit-me');
+    $(this).addClass('edit-me')
 });
 
-$('.color-button').each( function( i, elem ) {
-  var hueb = new Huebee( elem, {
-    // options
-    setBGColor: '.set-bg-elem'
+
+$( "button" ).on('click',function(){  
+    var size = 16;  
+    $(".controls .up").click(function(){  
+        size++;  
+        $("button").animate({fontSize: size});  
+    });  
+    $(".controls .down").click(function(){  
+        size--;  
+        $("button").animate({fontSize: size});  
+    });  
+});  
+
+
+
+$( ".table" ).on('click',function() {
+    $(".table").removeClass('me-table');
+    $(this).addClass('me-table')
+});
+
+
+$('#pickertext').on('change', function(){
+    $('.edit-me').css('color', $(this).val());
+})
+
+
+$('#pickerBorder').on('change', function(){
+    $('.me-table').css('background', $(this).val());
+    
+})
+
+
+$('#pickerBordertext').on('change', function(){
+    $('.me-table').css('color', $(this).val());
+    
+})
+
+
+$( "button" ).click(function() {
+  var html = [ "this are  the following styles:" ];
+ 
+  var styleProps = $( this ).css([
+    "width", "height", "color", "background-color",  "border-radius", "opacity", "border" ,"transition", "margin-right",  "border-right", "transform","box-shadow"
+  ]);
+  $.each( styleProps, function( prop, value ) {
+    html.push( prop + ": " + value + ";");
   });
+ 
+  $( "#result" ).html( html.join( "<br>" ) );
+});
+
+
+$( "table" ).click(function() {
+  var html = [ "this are  the following styles:" ];
+ 
+  var styleProps = $( this ).css([
+    "width", "height", "color", "background-color",  "border-radius", "opacity", "border",  "border-right", "focus","hover", "outline" ,"box-shadow"
+  ]);
+  $.each( styleProps, function( prop, value ) {
+    html.push( prop + ": " + value + ";");
+  });
+ 
+  $( "#result" ).html( html.join( "<br>" ) );
+});
+
+
+
+$(document).ready(function(){
+  $('button').on('click',function(){
+    $('button').removeClass("s");
+    $(this).addClass("s");
+});
 });
 
 
 
 
-
-$( document ).ready( function() {
-    var $body = $('.responsive-text'); //Cache this for performance
-
-    var setBodyScale = function() {
-        var scaleSource = $body.width(),
-            scaleFactor = 0.35,                     
-            maxScale = 600,
-            minScale = 30; //Tweak these values to taste
-
-        var fontSize = scaleSource * scaleFactor; //Multiply the width of the body by the scaling factor:
-
-        if (fontSize > maxScale) fontSize = maxScale;
-        if (fontSize < minScale) fontSize = minScale; //Enforce the minimum and maximums
-
-        $('.responsive-text').css('font-size', fontSize + '%');
-    }
-
-    $(window).resize(function(){
-        setBodyScale();
-    });
-
-    //Fire it when the page first loads:
-    setBodyScale();
+$(document).ready(function(){
+  $('#pickerShadow').on('change',function(){
+    $('.s').css({ 
+                 '-webkit-box-shadow' :
+                'inset 0 200px 400px -200px ' + $(this).val(),
+                '-moz-box-shadow': 'inset 0 200px 400px -200px '+ $(this).val(),
+                'box-shadow': '-1px 8px 15px 0px  '+ $(this).val() });
 });
+});
+
